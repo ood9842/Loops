05371833faaff88a5bc0abcbf2a2faca8654fdcc
ood9842 <ood9842@hotmail.com>
2016-11-11 01:30:59 +0700

    Try to create map

--- core/src/com/oop/loop/screen/Game_Frame.java
+++ core/src/com/oop/loop/screen/Game_Frame.java
@@ -4,8 +4,16 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.sun.corba.se.impl.oa.poa.ActiveObjectMap;
 
 /**
@@ -14,18 +22,33 @@ import com.sun.corba.se.impl.oa.poa.ActiveObjectMap;
 public class Game_Frame implements Screen {
     //window property
     private static final int SIZE = 600;
-    private static final int GRID_CELL = 40;
+    private static final int GRID_CELL = 30;
     //Graphic receive from Main class
     private SpriteBatch batch;
     //TOOL
     private ShapeRenderer shapeRenderer = new ShapeRenderer();
     private int count = 0;
+    //map
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthoCachedTiledMapRenderer paintMap;
+    //game camera to cooperation map
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
 
     public Game_Frame(SpriteBatch batch)
     {
+        //receive graphic from Loops class
         this.batch = batch;
+        //set camera and viewport
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(SIZE,SIZE,gameCam);
+        //set map
+        mapLoader =new TmxMapLoader();
+        map = mapLoader.load("map01.tmx");
+        paintMap = new OrthoCachedTiledMapRenderer(map);
+        gameCam.position.set(SIZE/2,SIZE/2,0);//SIZE window / 2 this is pattern
     }
-
 
     @Override
     public void show() {
@@ -34,8 +57,14 @@ public class Game_Frame implements Screen {
 
     @Override
     public void render(float delta) {
+        //set point to render map
+        paintMap.setView(gameCam);
+        //clear screen
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        //render at camera
+        batch.setProjectionMatrix(gameCam.combined);
+        paintMap.render();
         drawGrid();
         trick();//process
     }
@@ -49,7 +78,7 @@ public class Game_Frame implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        gamePort.update(width,height);
     }
 
     @Override
