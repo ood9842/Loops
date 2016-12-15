c7728e9d99292af851ff3ce02fc5cb883ee6a355
universestu <universestu@gmail.com>
2016-12-15 12:28:45 +0700

    ac chess and control map4

---
+++ core/src/com/oop/loop/mapScreen/Die_scene2.java
@@ -0,0 +1,68 @@
+package com.oop.loop.mapScreen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.TimeUtils;
+
+/**
+ * Created by Chetsada Chaiprasop on 12/14/2016.
+ */
+public class Die_scene2 implements Screen{
+    private Sound sound;
+    private SpriteBatch batch;
+    private Texture background;
+    private long time_start;
+
+    public Die_scene2(SpriteBatch batch)
+    {
+        this.batch = batch;
+        background = new Texture(Gdx.files.internal("scene\\die scene.jpg"));
+        sound = Gdx.audio.newSound(Gdx.files.internal("sound\\dead sound 2.wav"));
+    }
+
+    public void run()
+    {
+        time_start = TimeUtils.nanoTime();
+        sound.play();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        batch.begin();
+        batch.draw(background,0,0);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
